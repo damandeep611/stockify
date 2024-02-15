40d6a31709a2504f28e4da4bc81fe0af287d1a87
damandeep611 <damandeep611@gmail.com>
2024-02-15 19:31:27 +0530

    stockify-v1

---
+++ script.js
@@ -0,0 +1,22 @@
+// ===================responsive navbar 
+const hamburger = document.querySelector(".hamburger");
+  hamburger.onclick = function() {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+  }
+
+const primaryHeader = document.querySelector('.header');
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+  primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+},{rootMargin: "200px 0px 0px 0px"});
+navObserver.observe(scrollWatcher)
+
+
+
+
+
