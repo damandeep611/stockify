7f571d08a70e8f4531d2662167bf8d0377dee0e9
damandeep611 <damandeep611@gmail.com>
2024-02-19 19:03:56 +0530

    updated layout and responsiveness with padding and images

--- script.js
+++ script.js
@@ -7,17 +7,7 @@ const hamburger = document.querySelector(".hamburger");
 
 
   // ================change color on scroll header with sticky
-const primaryHeader = document.querySelector('.header');
-const scrollWatcher = document.createElement('div');
-
-scrollWatcher.setAttribute('data-scroll-watcher', '');
-primaryHeader.before(scrollWatcher);
-
-const navObserver = new IntersectionObserver((entries) => {
-  primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
-},{rootMargin: "200px 0px 0px 0px"});
-navObserver.observe(scrollWatcher)
-
+// 
 
 
 
